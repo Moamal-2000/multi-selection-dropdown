bfcb013a34a5feb4603023423b604c5c1cf7aa6b
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2024-10-14 12:17:28 +0300

    test: include all checkedNodes in selectedItems

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import MultiSelectDropdown from "./Components/MultiSelectDropdown/MultiSelectDropdown";
 import SelectionInput from "./Components/MultiSelectDropdown/SelectionInput/SelectionInput";
 import { dropDownData } from "./Data/dropdownData";
+import { findNodeById } from "./Functions/helper";
 
 function App() {
   const [checkedState, setCheckedState] = useState({
@@ -10,10 +11,14 @@ function App() {
     selectedItems: [],
   });
 
+  const selectedItems = checkedState.checkedNodes.map((id) =>
+    findNodeById(dropDownData, id)
+  );
+
   return (
     <div className="App">
       <SelectionInput
-        selectedItems={checkedState.selectedItems}
+        selectedItems={selectedItems}
         placeholder="Choose..."
       />
       <MultiSelectDropdown
